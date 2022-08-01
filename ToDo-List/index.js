474548de89b986eefdba0912ab3f09c831f6941e
batikan <emiruar123@gmail.com>
2022-08-01 21:31:46 +0300

    todo-list last-part

--- ToDo-List/index.js
+++ ToDo-List/index.js
@@ -4,13 +4,12 @@ let buttonDOM = document.querySelector("#liveToastBtn");
 let successAlertDOM = document.querySelector("#successToast");
 let dangerAlertDOM = document.querySelector("#dangerToast");
 
-let todoListDOM=document.querySelector(".list-group");
+let todoListDOM = document.querySelector(".list-group");
+let todoListDOMItem = document.querySelector(".list-group-item");
 
-
-
-
-buttonDOM.addEventListener('click', addNewElement)
-todoListDOM.addEventListener('click',Check)
+buttonDOM.addEventListener("click", addNewElement);
+todoListDOM.addEventListener("click", Check);
+//todoListitemDOM.addEventListener("click",Checked);
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -29,49 +28,61 @@ function addNewElement() {
     liDOM.classList.add("list-group-item");
     ulDOM.append(liDOM);
     liDOM.append(btnDOM);
-    inputDOM.value="";
+    inputDOM.value = "";
   }
 }
 
-
-function Check(e){
-    const item = e.target;
-    //? If click remove button -> remove li element
-    if(item.classList[0] == 'btn-close'){
-        const el = item.parentElement;
-        //DeleteLocalStorage(el.innerText) //* Fonksiyona silmek istediğimiz li elemanının text değerini gönderdik. 
-        el.classList.add("animation");
-        el.classList.remove('list-group-item')
-        el.addEventListener("transitionend",function(){
-            el.remove()
-        })
-        
-    }
-    //? If click li element -> toggle checked class -- toggle ile belirttiğimiz class elementte varsa siler, eğer yoksa ekler --
-    else{
-        //  item.classList.toggle('checked')
-    }
+function Check(e) {
+  const item = e.target;
+  //? If click remove button -> remove li element
+  if (item.classList[0] == "btn-close") {
+    const el = item.parentElement;
+    //DeleteLocalStorage(el.innerText) //* Fonksiyona silmek istediğimiz li elemanının text değerini gönderdik.
+    el.classList.add("animation");
+    el.classList.remove("list-group-item");
+    el.addEventListener("transitionend", function () {
+      el.remove();
+    });
+  }
+  //? If click li element -> toggle checked class -- toggle ile belirttiğimiz class elementte varsa siler, eğer yoksa ekler --
+  else {
+    item.classList.toggle("checked");
+  }
 }
 
+function clearAll() {
+  let listItems = document.querySelectorAll(".list-group-item");
+  listItems.forEach((p) => {
+    p.classList.add("animation");
+  });
+}
 
+function markAll() {
+  let listItems = document.querySelectorAll(".list-group-item");
+  listItems.forEach((p) => {
+    p.classList.add("checked");
+  });
+}
 
+function checked() {
+  todoListitemDOM.classList.add("checked");
+  if (todoListDOM.classList.includes("checked")) {
+    todoListDOM.classList.toggle("checked");
+  }
+}
 
-
-
-
-function ToastAlertSuccess(){
-    let successToast = new bootstrap.Toast(successAlertDOM, alertOptions)
-    successToast.show();
+function ToastAlertSuccess() {
+  let successToast = new bootstrap.Toast(successAlertDOM, alertOptions);
+  successToast.show();
 }
 
 //* Danger Alerts
-function ToastAlertDanger(){
-    let dangerToast = new bootstrap.Toast(dangerAlertDOM, alertOptions)
-    dangerToast.show();
+function ToastAlertDanger() {
+  let dangerToast = new bootstrap.Toast(dangerAlertDOM, alertOptions);
+  dangerToast.show();
 }
-
 
 let alertOptions = {
-    animation: true,
-    delay: 1000,
-}
+  animation: true,
+  delay: 1000,
+};
